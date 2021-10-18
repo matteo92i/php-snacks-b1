0f364fdd667ae40ee952122294144f8895439f09
Matteo92i <matteo.iovane@gmail.com>
2021-10-18 16:38:02 +0200

    Sncak4

---
+++ Snack4.php
@@ -0,0 +1,65 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+
+
+
+<?php foreach($posts as $data => $post) { ?>
+    <h1> <?php echo $data?></h1>
+    <ul>
+    <?php foreach($post as $element) { ?>
+        <li> 
+            <?php echo $element ['title']; ?> 
+        </li>
+        <li>
+            <?php echo $element ['author']; ?> 
+        </li>
+        <li>
+            <?php echo $element ['text']; ?> 
+        </li>
+        
+    <?php } ?>
+
+    </ul>
+
+<?php } ?>  
